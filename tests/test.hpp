9d4b16ceca1f6795b22f779e3a0e407875f7b81b
Mikhail Lozhnikov <lozhnikovma@gmail.com>
2025-02-27 18:52:14 +0300

    Created geometry2025 repo on the basis of geometry2022 repo.

---
+++ tests/test.hpp
@@ -0,0 +1,39 @@
+/**
+ * @file tests/test.hpp
+ * @author Mikhail Lozhnikov
+ *
+ * Заголовочный файл для объявлений основных тестовых функций.
+ */
+
+#ifndef TESTS_TEST_HPP_
+#define TESTS_TEST_HPP_
+
+#include <httplib.h>
+
+/**
+ * @brief Набор тестов для класса geometry::Point.
+ */
+void TestPoint();
+
+/**
+ * @brief Набор тестов для класса geometry::Edge.
+ */
+void TestEdge();
+
+/**
+ * @brief Набор тестов для класса geometry::Polygon.
+ */
+void TestPolygon();
+
+/**
+ * @brief Набор тестов для функции geometry::Orientation().
+ */
+void TestOrientation();
+
+/* Сюда нужно добавить объявления тестовых функций. */
+
+
+
+/* Конец вставки. */
+
+#endif  // TESTS_TEST_HPP_
