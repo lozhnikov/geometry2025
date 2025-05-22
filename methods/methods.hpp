b80c535649cd94e6f021108838617b8d395d5180
Аджинджал Беслан <nikaa2172005@yandex.ru>
2025-05-22 21:49:05 +0300

    Исправлены неточности 3

--- methods/methods.hpp
+++ methods/methods.hpp
@@ -1,20 +1,19 @@
 /**
  * @file methods/methods.hpp
- * @author Nika Adzhindzhal
- *
- * Объявление функции для алгоритма построения звездчатого многоугольника.
+ * @author Mikhail Lozhnikov
+ * 
+ * Объявления функий для серверной части алгоритмов построения звездчатого многоугольника.. Эти функции должны
+ * считать JSON, который прислал клиент, выполнить алгоритм и отправить клиенту
+ * JSON с результатом работы алгоритма.
  */
 
 #ifndef METHODS_METHODS_HPP_
 #define METHODS_METHODS_HPP_
 
-#include <nlohmann/json.hpp>
-
 namespace geometry {
 
 	int My_PoligMethod(const nlohmann::json& input, nlohmann::json* output);
 
-
 } // namespace geometry
 
-#endif // METHODS_METHODS_HPP_
+#endif// METHODS_METHODS_HPP_
