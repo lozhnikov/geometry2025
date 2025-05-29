bd35be1f165ddf51977c06670376e1396a81a80a
Назаров Сергей Алексеевич <f00lingloves@gmail.com>
2025-05-29 18:20:13 +0300

    Update main.cpp

--- methods/main.cpp
+++ methods/main.cpp
@@ -37,26 +37,6 @@ int main(int argc, char* argv[]) {
 
   /* Сюда нужно вставить обработчик post запроса для алгоритма. */
 
-  svr.Post("/GrahamScan",
-           [&](const httplib::Request& req, httplib::Response& res) {
-    try {
-      auto input = json::parse(req.body);
-      json output;
-
-      int result = geometry::GrahamScanMethod(input, &output);
-
-      if (result != 0) {
-        res.status = 400;  // Bad request
-      }
-
-      res.set_content(output.dump(), "application/json");
-    } catch (const std::exception& e) {
-      json error_output = {{"error", std::string("Parse error: ") + e.what()}};
-      res.status = 400;
-      res.set_content(error_output.dump(), "application/json");
-    }
-  });
-
   svr.Post("/SutherlandHodgman",
            [&](const httplib::Request& req, httplib::Response& res) {
     try {
